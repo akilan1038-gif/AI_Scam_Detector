b778cc429cc7bb1d93d0d0d9b5e60c40f2963605
akilan1038-gif <akilan1038@gmail.com>
2026-07-26 12:03:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def predict():
     return render_template("result.html", prediction=prediction, message=message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
